ac1f748ac3cf3d4ba50cefb3fe7c159d1b620036
Marcone Tenorio <marconetsf@gmail.com>
2022-08-11 17:25:52 -0300

    aula 2

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    const [valor, setValor] = useState('')
+    
+    const aoDigitado = (event) => {
+        setValor(event.target.value);
+        console.log(event.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatiorio} placeholder={props.placeholder}></input>
+            <input onChange={aoDigitado} required={props.obrigatiorio} placeholder={props.placeholder}></input>
         </div>
     )
 }
