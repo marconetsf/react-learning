171c95a1a22fe18d2bfbe8eb5bd4f04a0ff3f70d
Marcone Tenorio <marconetsf@gmail.com>
2022-08-11 10:19:18 -0300

    aula 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Banner from './componentes/Banner';
-import CampoTexto from './componentes/CampoTexto/CampoTexto';
 import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner></Banner>
-      <CampoTexto label="Nome" placeholder="Digite seu Nome"></CampoTexto>
-      <CampoTexto label="Cargo" placeholder="Digite seu Cargo"></CampoTexto>
-      <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"></CampoTexto>
+      <Formulario></Formulario>
     </div>
   );
 }
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,5 +1,15 @@
+import CampoTexto from '../CampoTexto/CampoTexto'
 import './Formulario.css'
 
 export const Formulario = () => {
-    
+    return (
+        <section className='formulario'>
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto label="Nome" placeholder="Digite seu Nome"></CampoTexto>
+                <CampoTexto label="Cargo" placeholder="Digite seu Cargo"></CampoTexto>
+                <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"></CampoTexto>
+            </form>
+        </section>
+    )
 }
